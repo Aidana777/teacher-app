01d8cec3ff961b87323c1cec62b333eb4a0efebf
Aidana777 <aidananiazalieva27@gmail.com>
2023-11-20 19:29:01 +0600

    Form

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,27 +6,50 @@ const Home = () => {
   const [isAddTeacherFormVisible, setIsAddTeacherFormVisible] = useState(false);
   const [teacherNotFound, setTeacherNotFound] = useState(false);
   const [selectedUniversity, setSelectedUniversity] = useState('');
+  const [newTeacherData, setNewTeacherData] = useState({
+    name: '',
+    surname: '',
+    patronymic: '',
+    university: '',
+    position: '',
+    photo: null,
+  });
 
   const handleUniversityButtonClick = (university) => {
     // Implement the logic to check if the teacher for the selected university is in the database
-    const teacherFound = true; // Replace with actual logic
+    const teacherFound = false; // Replace with actual logic
 
     if (teacherFound) {
       setTeacherNotFound(false);
     } else {
       setTeacherNotFound(true);
       setIsAddTeacherFormVisible(true);
+      setSelectedUniversity(university);
     }
-
-    // Update the selected university
-    setSelectedUniversity(university);
   };
 
   const handleAddTeacherSubmit = (event) => {
     event.preventDefault();
-    // Implement the logic to handle the submission of the teacher form
+    // Implement the logic to handle the submission of the new teacher form
     // You may want to send the data to the server, update the database, etc.
+    console.log('New teacher data:', newTeacherData);
     setIsAddTeacherFormVisible(false);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewTeacherData({
+      ...newTeacherData,
+      [name]: value,
+    });
+  };
+
+  const handleFileChange = (event) => {
+    const photoFile = event.target.files[0];
+    setNewTeacherData({
+      ...newTeacherData,
+      photo: photoFile,
+    });
   };
 
   return (
@@ -61,19 +84,41 @@ const Home = () => {
           No teacher found for the selected university. You can add a new teacher:
         </div>
       )}
-
-      {/* Add Teacher Section */}
       {isAddTeacherFormVisible && (
         <div className="addTeacher">
           <form onSubmit={handleAddTeacherSubmit}>
             <label>Name:</label>
-            <input type="text" required />
+            <input
+              type="text"
+              name="name"
+              value={newTeacherData.name}
+              onChange={handleInputChange}
+              required
+            />
             <label>Surname:</label>
-            <input type="text" required />
+            <input
+              type="text"
+              name="surname"
+              value={newTeacherData.surname}
+              onChange={handleInputChange}
+              required
+            />
             <label>Patronymic:</label>
-            <input type="text" required />
+            <input
+              type="text"
+              name="patronymic"
+              value={newTeacherData.patronymic}
+              onChange={handleInputChange}
+              required
+            />
             <label>University:</label>
-            <select required>
+            <select
+              name="university"
+              value={newTeacherData.university}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="" disabled>Select University</option>
               {universitiesInBishkek.map((university, index) => (
                 <option key={index} value={university}>
                   {university}
@@ -81,10 +126,21 @@ const Home = () => {
               ))}
             </select>
             <label>Position:</label>
-            <input type="text" required />
+            <input
+              type="text"
+              name="position"
+              value={newTeacherData.position}
+              onChange={handleInputChange}
+              required
+            />
             <label>Attach Photo:</label>
-            <input type="file" accept="image/*" />
-            <button type="submit">Submit</button>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+              required
+            />
+            <button type="submit">Добавить</button>
           </form>
         </div>
       )}
